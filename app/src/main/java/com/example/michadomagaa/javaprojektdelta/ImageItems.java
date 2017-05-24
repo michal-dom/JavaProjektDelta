1bf0fe56b9ba9846616e4b8510103a8439b165a5
Marcin Dyrcz <mdyrcz4@gmail.com>
2017-05-24 15:17:31 +0200

    ImageItems class

---
+++ app/src/main/java/com/example/michadomagaa/javaprojektdelta/ImageItems.java
@@ -0,0 +1,40 @@
+package com.example.michadomagaa.javaprojektdelta;
+
+import android.graphics.Bitmap;
+/**
+ * Created by Marcin on 24.05.2017.
+ */
+
+public class ImageItems {
+
+    private Bitmap image;
+    private String title;
+    private long size;
+
+    public ImageItems(Bitmap image, String title, long size){
+        super();
+        this.image = image;
+        this.title = title;
+        this.size = size;
+    }
+
+    public Bitmap getImage(){
+        return image;
+    }
+
+    public void setImage(){
+        this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public long getSize() {
+        return size;
+    }
+}
