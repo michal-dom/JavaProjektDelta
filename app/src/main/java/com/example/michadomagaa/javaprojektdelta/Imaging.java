cc1149473b5e29306c10e893631847ab669597a9
michal-dom <michal-domagala@hotmail.com>
2017-05-25 11:05:14 +0200

    czwarty

--- app/src/main/java/com/example/michadomagaa/javaprojektdelta/Imaging.java
+++ app/src/main/java/com/example/michadomagaa/javaprojektdelta/Imaging.java
@@ -8,6 +8,8 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 public class Imaging {
 
@@ -21,6 +23,31 @@ public class Imaging {
 
         File file = new File(path);
         bitmap = BitmapFactory.decodeFile(file.getAbsolutePath(), options);
-        return bitmap;
+        return decodeFile(file) ;
+    }
+    private static Bitmap decodeFile(File f) {
+        try {
+            // Decode image size
+            BitmapFactory.Options o = new BitmapFactory.Options();
+            o.inJustDecodeBounds = true;
+            BitmapFactory.decodeStream(new FileInputStream(f), null, o);
+
+            // The new size we want to scale to
+            final int REQUIRED_SIZE=70;
+
+            // Find the correct scale value. It should be the power of 2.
+            int scale = 1;
+            while(o.outWidth / scale / 2 >= REQUIRED_SIZE &&
+                    o.outHeight / scale / 2 >= REQUIRED_SIZE) {
+                scale *= 2;
+            }
+
+            // Decode with inSampleSize
+            BitmapFactory.Options o2 = new BitmapFactory.Options();
+            o2.inSampleSize = scale;
+            return BitmapFactory.decodeStream(new FileInputStream(f), null, o2);
+        } catch (FileNotFoundException e) {}
+        return null;
     }
 }
+
