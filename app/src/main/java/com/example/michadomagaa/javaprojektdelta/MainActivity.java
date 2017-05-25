4612324d9c8369143e74280f4b11433ece81c420
michal-dom <michal-domagala@hotmail.com>
2017-05-25 10:57:46 +0200

    trzeci

--- app/src/main/java/com/example/michadomagaa/javaprojektdelta/MainActivity.java
+++ app/src/main/java/com/example/michadomagaa/javaprojektdelta/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.michadomagaa.javaprojektdelta;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private TextView tv1, tv2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv1 = (TextView) findViewById(R.id.main_gallery);
+        tv1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(
+                        MainActivity.this,
+                        GalleryActivity.class);
+                intent.putExtra("root", "root");
+                startActivity(intent);
+            }
+        });
+
+        tv2 = (TextView) findViewById(R.id.main_camera);
+        tv2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(
+                        MainActivity.this,
+                        GalleryActivity.class);
+                intent.putExtra("root", "root");
+                startActivity(intent);
+            }
+        });
     }
+
+
 
 }
